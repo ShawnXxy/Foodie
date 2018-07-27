285c1556f904ce1d4584625c9408e6b331d44d2d
ShawnXxy <shawnxxy@hotmail.com>
2018-07-27 15:03:27 +0800

    updates

--- src/db/mysql/DBUtil.java
+++ src/db/mysql/DBUtil.java
@@ -12,10 +12,10 @@ public class DBUtil {
 //    public static final String URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME + "&password=" + PASSWORD + "&autoreconnect=true";
     
     // Azure Database for MySQL
-    private static final String HOSTNAME = "az-foodie-db.mysql.database.azure.com"; // az-foodie-db.mysql.database.azure.com 52.175.33.150
+    private static final String HOSTNAME = "az-foodie-mydb.mysql.database.azure.com"; // az-foodie-db.mysql.database.azure.com 52.175.33.150
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "foodie";
-    static final String USERNAME = "x_xiao2_bnu@az-foodie-db";
+    static final String USERNAME = "x_xiao2_bnu@az-foodie-mydb";
     static final String PASSWORD = "XXy@4592995";
     public static final String URL = String.format("jdbc:mysql://%s/%s", HOSTNAME, DB_NAME);
     
